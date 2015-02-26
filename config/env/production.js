8dad4a3d9385a2f39dc0a320bffff13f186f3bc0
ftorletti <frantorletti@gmail.com>
2015-02-26 19:55:31 -0300

    second commit

---
+++ config/env/production.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  //db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-prod',
+  db: 'mongodb://<oscarevangelista5@gmail.com>:<desarrollo1>@ds049631.mongolab.com:49631/oscar',
+  /**
+   * Database options that will be passed directly to mongoose.connect
+   * Below are some examples.
+   * See http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html#mongoclient-connect-options
+   * and http://mongoosejs.com/docs/connections.html for more information
+   */
+  dbOptions: {
+    /*
+    server: {
+        socketOptions: {
+            keepAlive: 1
+        },
+        poolSize: 5
+    },
+    replset: {
+      rs_name: 'myReplicaSet',
+      poolSize: 5
+    },
+    db: {
+      w: 1,
+      numberOfRetries: 2
+    }
+    */
+  },
+  app: {
+    name: 'Logistica Oscar'
+  },
+  facebook: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+  },
+  twitter: {
+    clientID: 'CONSUMER_KEY',
+    clientSecret: 'CONSUMER_SECRET',
+    callbackURL: 'http://localhost:3000/auth/twitter/callback'
+  },
+  github: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/github/callback'
+  },
+  google: {
+    clientID: 'APP_ID',
+    clientSecret: 'APP_SECRET',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  linkedin: {
+    clientID: 'API_KEY',
+    clientSecret: 'SECRET_KEY',
+    callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+  },
+  emailFrom: 'oscarevangelista5@gmail.com', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'Gmail', // Gmail, SMTP
+    auth: {
+      user: 'oscarevangelista5@gmail.com',
+      pass: 'desarrollo1'
+    }
+  }
+};
---
+++ packages/users/server/template.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  forgot_password_email: function(user, req, token, mailOptions) {
+    mailOptions.html = [
+      'Hi ' + user.name + ',',
+      'We have received a request to reset the password for your account.',
+      'If you made this request, please click on the link below or paste this into your browser to complete the process:',
+      'http://' + req.headers.host + '/#!/reset/' + token,
+      'This link will work for 1 hour or until your password is reset.',
+      'If you did not ask to change your password, please ignore this email and your account will remain unchanged.'
+    ].join('\n\n');
+    mailOptions.subject = 'Resetting the password';
+    return mailOptions;
+  },
+  contact: function(client, req, mailOptions) {
+    mailOptions.html = [
+      'NOMBRE: '+ client.name,
+      'EMAIL: '+ client.email,
+      'ASUNTO: ' + client.subject,
+      'CONTENIDO: '+ client.content
+    ].join('\n\n');
+    mailOptions.subject = '***EMAIL DE CONTACTO***';
+    return mailOptions;
+  }
+};
